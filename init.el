fa3dd7d73622a34dd89dbdba91525e01e1fb4afe
Nikita Mistyukov <nekifirus@gmail.com>
2018-09-21 23:08:25 +0500

    add counsel keybindings and ag!

--- init.el
+++ init.el
@@ -21,6 +21,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(ag-highlight-search t)
  '(calendar-week-start-day 1)
  '(custom-safe-themes
    (quote
