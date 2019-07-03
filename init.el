5a1ef5539971797e4c1cd7a1e2ed3a66485a752b
Nikita Mistyukov <nekifirus@gmail.com>
2019-07-04 02:05:01 +0500

    last changes

--- init.el
+++ init.el
@@ -16,3 +16,26 @@
     (org-babel-load-file (expand-file-name "README.org" user-emacs-directory))))
 
 ;;; init.el ends here
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ag-highlight-search t)
+ '(calendar-week-start-day 1)
+ '(copy-as-format-default "slack" t)
+ '(face-font-family-alternatives (quote (("Consolas" "Monaco" "Monospace"))))
+ '(google-translate-default-source-language "en" t)
+ '(google-translate-default-target-language "ru" t)
+ '(no-littering-var-directory "/Users/Nekifirus/.emacs.d/data/" t)
+ '(projectile-completion-system (quote ivy))
+ '(send-mail-function (quote mailclient-send-it))
+ '(show-paren-delay 0.0)
+ '(system-packages-noconfirm t))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(trailing-whitespace ((t (:background "dim gray")))))
+(put 'narrow-to-region 'disabled nil)
