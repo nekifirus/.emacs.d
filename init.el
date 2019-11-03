e414008ec81c8cf1509c112bf1597968404f9dd0
Nikita Mistyukov <nekifirus@gmail.com>
2019-11-03 23:24:52 +0500

    add isort disable elpy highlight indentation

--- init.el
+++ init.el
@@ -21,13 +21,14 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(ag-highlight-search t t)
+ '(ag-highlight-search t)
  '(calendar-week-start-day 1)
- '(copy-as-format-default "slack" t)
+ '(copy-as-format-default "slack")
  '(face-font-family-alternatives (quote (("Consolas" "Monaco" "Monospace"))))
- '(google-translate-default-source-language "en")
- '(google-translate-default-target-language "ru")
+ '(google-translate-default-source-language "en" t)
+ '(google-translate-default-target-language "ru" t)
  '(no-littering-var-directory "/home/nekifirus/.emacs.d/data/" t)
+ '(org-agenda-files (quote ("~/org/gtd.org" "~/org/refile.org")))
  '(package-selected-packages
    (quote
     (smart-comment ace-window avy-zap avy expand-region whole-line-or-region smartparens rainbow-mode rainbow-identifiers rainbow-delimiters cider clojure-snippets clojure-mode-extra-font-locking clojure-mode flycheck-mix alchemist elixir-mode company-statistics company all-the-icons-ivy all-the-icons-dired all-the-icons base16-theme google-translate google-this darkroom plantuml-mode copy-as-format yasnippet-snippets yasnippet haml-mode direnv htmlize no-littering docker-compose-mode dockerfile-mode docker ag csv-mode markdown-mode reverse-im wakatime-mode restart-emacs exec-path-from-shell toc-org org-plus-contrib gist magit-gh-pulls diff-hl gitignore-mode counsel-projectile ivy which-key diminish use-package-ensure-system-package system-packages use-package nix-mode magit)))
